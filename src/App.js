3cba04e61d1e0e0f60faab8831d0839ad40f28a5
Zach Banducci <zbducci@gmail.com>
2019-04-09 11:56:20 -0500

    Home component and home route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Home from './pages/Home'
 import Room from './pages/Room'
 import NotFound from './pages/NotFound'
 
@@ -8,6 +9,7 @@ import './App.css';
 
 const App = () => (
   <Switch>
+    <Route exact path='/' component={Home} /> 
     <Route path='/:id' component={Room} />
     <Route component={NotFound} />
   </Switch>
