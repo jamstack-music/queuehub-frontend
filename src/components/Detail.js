3cc55995c4ca23314b788e60d98b9c83a2e173ac
Zach Banducci <zbducci@gmail.com>
2019-04-15 12:16:47 -0500

    Detail component now enclosed in a fragment

---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Detail = styled.td`
+  font-size: 14px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  height: 4em;
+  padding: 0 0.5em;
+`
+
+export default Detail
