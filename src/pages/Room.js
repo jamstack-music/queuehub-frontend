1b8cf2a575ef1a763159c43926708a305e7dd289
Zach Banducci <zbducci@gmail.com>
2019-04-16 23:56:41 -0500

    Connected Room Page to store

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import { NavLink, Route, Switch } from 'react-router-dom'
 import { 
   IoIosSearch as SearchIcon,
@@ -6,11 +6,11 @@ import {
   IoMdPeople as MembersIcon
 } from 'react-icons/io'
 
-import axios from 'axios'
 import styled from 'styled-components'
-
+import { RoomContainer } from '../store/room'
+import { Subscribe } from 'unstated'
 import Nav from '../components/Nav'
-
+import StoreMiddleWare from '../containers/StoreMiddleWare'
 import CurrentPlaying from './CurrentPlaying'
 import Members from './Members'
 import Search from './Search'
@@ -22,39 +22,28 @@ const View = styled.div`
 `
 
 const Room = ({ match }) => { 
-  let eventSource = useRef(null) 
-
-  useEffect(function init() {
-    eventSource = new EventSource('http://34.219.153.198:5000/stream')
-
-    eventSource.addEventListener('greeting', function(event) {
-      alert('member added')
-    }, false)
-
-    axios.get('http://34.219.153.198:5000/create/100').then(res => {
-      console.log('Store connected')
-    })
-
-    return function unMount() {
-      eventSource.removeAllListeners()
-      eventSource.close()
-    }
-  },[])
-
   return (
-    <View>
-      <Nav>
-        <NavLink exact to={`${match.url}`}><CurrentPlayingIcon size='1.5em'/></NavLink>
-        <NavLink to={`${match.url}/members`}><MembersIcon size='1.5em'/></NavLink>
-        <NavLink to={`${match.url}/search`}><SearchIcon size='1.5em'/></NavLink>
-      </Nav>
-      <Switch>
-        <Route exact path={`${match.url}`} component={CurrentPlaying} />
-        <Route path={`${match.url}/members`} component={Members} />
-        <Route path={`${match.url}/search`} component={Search} />
-        <Route component={NotFound} /> 
-      </Switch>
-    </View>
+    <Subscribe to={[RoomContainer]}>
+      {
+        room => (
+          <StoreMiddleWare room={room} roomID={match.params.id}>
+            <View>
+              <Nav>
+                <NavLink exact to={`${match.url}`}><CurrentPlayingIcon size='1.5em'/></NavLink>
+                <NavLink to={`${match.url}/members`}><MembersIcon size='1.5em'/></NavLink>
+                <NavLink to={`${match.url}/search`}><SearchIcon size='1.5em'/></NavLink>
+              </Nav>
+              <Switch>
+                <Route exact path={`${match.url}`} component={CurrentPlaying} />
+                <Route path={`${match.url}/members`} component={Members} />
+                <Route path={`${match.url}/search`} component={Search} />
+                <Route component={NotFound} /> 
+              </Switch>
+            </View>
+          </StoreMiddleWare>
+        )
+      }
+    </Subscribe>
   )
 }
 
