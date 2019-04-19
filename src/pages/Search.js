df2d90a067ccc559a0d368db1a99175767a5ba78
Zach Banducci <zbducci@gmail.com>
2019-04-19 17:42:36 -0500

    Search page now has duplicate detection

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,11 +6,34 @@ import { RoomContainer } from '../store/room'
 import { addSong } from '../data/api'
 import SearchList from '../components/SearchList'
 import SearchBar from '../components/SearchBar'
+import { Subscribe } from 'unstated'
+import { RoomContainer } from '../store/room'
+import { addSong as addSongRemote } from '../data/api'
+import Alert from '../components/Alert'
+
+const renderAlert = (alert) => {
+  if(alert === 'success') {
+    return <Alert type='success' message='Song Added!' />
+  } else if(alert === 'failure') {
+    console.log('triggered')
+    return <Alert type='failure' message='Looks like the song is already in the queue' />
+  }
+}
 
 const Search = () => {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
-
+  const [alert, setAlert] = useState(null)
+  
+  const addSong = (room, song) => {
+    if(room.state.queue.find(({ id }) => id === song.id)) {
+      setAlert('failure')
+    } else{
+      addSongRemote(room.state.id, song).then(res => {
+        setAlert('success')
+      })
+    }
+  }
   useEffect(function search() {
     spotify.searchTracks(query).then(results => {
       const { tracks: { items } } = results
@@ -48,9 +71,10 @@ const Search = () => {
       {
         room => (
           <div>
+            { renderAlert(alert) }
             <SearchBar onChange={e => setQuery(e.target.value)}/>
             <SearchList
-              onAdd={song => addSong(room.state.name, song)}
+              onAdd={song => addSong(room, song)}
               style={{ marginTop: '3em' }}
               songs={results} />  
           </div>
