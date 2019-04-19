9c7c616af2061e2c5a940764e36fcab99d8d42dc
Zach Banducci <zbducci@gmail.com>
2019-04-19 14:20:34 -0500

    Infinite scroll hook

---
+++ src/hooks/useInfiniteRetrieval.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import { getNext } from '../data/spotify'
+
+
+export default function useInfiniteRetrieval(initial) {
+  const [next, setNext] = useState(initial)
+  const [loading, setLoading] = useState(true)
+  const [list, setList] = useState([])
+  
+  const handleScroll = () => {
+    if(
+      (window.innerHeight + window.scrollY) >= 
+      (document.body.offsetHeight - 2000) 
+    ) {
+      setLoading(true)
+    }
+  }
+  useEffect(function windowListener() {
+    window.addEventListener('scroll', () => handleScroll(), false)
+
+    return function unListen() { 
+      window.removeEventListener('scroll', () => handleScroll(), false)
+    }
+  }, [])
+
+  useEffect(function fetch() {
+    if(loading){
+      getNext(next).then(res => {
+        const { items, next } = res
+        const newList = items.map(item => {
+          if(item.album) return item.album
+          return item
+        })
+        
+        setList([...list, ...newList])
+        setLoading(false)
+        setNext(next)
+      })    
+
+    }
+  }, [loading])
+  
+  return [list, loading] 
+}
+
