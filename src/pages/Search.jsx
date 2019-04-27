25ed9932cb3336a4a6c7b98834c6cf49f3ceec96
Zach Banducci <zbducci@gmail.com>
2019-04-27 11:09:05 -0500

    Restructured Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,57 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { Subscribe } from 'unstated';
 import { useAlert } from 'react-alert';
 
 import { addSong as addSongRemote } from '../data/api';
 import extractSong from '../data/extractors/song';
 import { spotify } from '../data/spotify';
-import { RoomContainer } from '../store/room';
 
 import AddList from '../components/Songs/AddList';
 import SearchBar from '../components/SearchBar';
 
-const Search = () => {
+const addSong = (alert, room, song) => {
+  if (room.state.queue.find(({ id }) => id === song.id)) {
+    alert.error('This song is already in the queue!');
+  } else {
+    addSongRemote(room.state.name, song).then(() => {
+      alert.success('Song added to queue!');
+    });
+  }
+};
+
+const searchSpotify = (query, setResults) => {
+  if (query === '') {
+    setResults([]);
+    return;
+  }
+
+  spotify.searchTracks(query, { market: 'US' }).then((res) => {
+    const { tracks: { items } } = res;
+
+    const searchResults = items.map(song => extractSong(song));
+    setResults(searchResults);
+  }).catch(({ status }) => {
+    if (status === 400) setResults([]);
+  });
+};
+
+const Search = (props) => {
+  const {
+    room,
+  } = props;
+
   const alert = useAlert();
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
 
-  const addSong = (room, song) => {
-    if (room.state.queue.find(({ id }) => id === song.id)) {
-      alert.error('This song is already in the queue!');
-    } else {
-      addSongRemote(room.state.name, song).then(() => {
-        alert.success('Song added to queue!');
-      });
-    }
-  };
-
   useEffect(() => {
-    spotify.searchTracks(query, { market: 'US' }).then((res) => {
-      const { tracks: { items } } = res;
-
-      const searchResults = items.map(song => extractSong(song));
-
-      setResults(searchResults);
-    }).catch(({ status }) => {
-      if (status === 400) setResults([]);
-    });
+    searchSpotify(query, setResults);
   }, [query]);
 
+  const handleAdd = song => addSong(alert, room, song);
   return (
-    <Subscribe to={[RoomContainer]}>
-      {
-        room => (
-          <div>
-            <SearchBar onChange={e => setQuery(e.target.value)} />
-            <AddList
-              onAdd={song => addSong(room, song)}
-              style={{ marginTop: '3em' }}
-              songs={results}
-            />
-          </div>
-        )
-      }
-    </Subscribe>
+    <div>
+      <SearchBar onChange={setQuery} />
+      <AddList
+        onAdd={handleAdd}
+        style={{ marginTop: '3em' }}
+        songs={results}
+      />
+    </div>
   );
 };
 
