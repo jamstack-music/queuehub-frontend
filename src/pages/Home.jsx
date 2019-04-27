ba7934b04ae83de60ac985170e98566e33e6e39a
Zach Banducci <zbducci@gmail.com>
2019-04-26 19:07:41 -0500

    Form input component and fixed pointless rerendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,25 +4,7 @@ import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAlert } from 'react-alert';
 
-const FormInput = styled.div`
-  margin-bottom: 1em;
-  
-  & * {
-    display: block;
-  }
-`;
-
-const Input = styled.input`
-  font-size: 16px;
-  border: 1px grey solid;
-  padding: 0.5em;
-  border-radius: 25px;
-`;
-
-const Label = styled.label`
-  font-size: 18px;
-  margin: 0.5em;
-`;
+import FormInput from '../components/FormInput';
 
 const Button = styled.button`
   margin: 2em;
@@ -64,14 +46,18 @@ const Home = (props) => {
     >
       <h1>Queuehub</h1>
       <form onSubmit={() => setSubmit(true)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <FormInput>
-          <Label htmlFor="room-name">Enter in room name</Label>
-          <Input type="text" id="room-name" onChange={e => setRoom(e.target.value)} />
-        </FormInput>
-        <FormInput>
-          <Label htmlFor="username">Enter a name</Label>
-          <Input type="text" id="username" onChange={e => setName(e.target.value)} />
-        </FormInput>
+        <FormInput
+          label="Enter a room name"
+          id="room-name"
+          onChange={setRoom}
+          value={room}
+        />
+        <FormInput
+          label="Enter your name"
+          id="name"
+          onChange={setName}
+          value={name}
+        />
         <Button disabled={!room || !name} type="submit">Let's go!</Button>
       </form>
     </div>
