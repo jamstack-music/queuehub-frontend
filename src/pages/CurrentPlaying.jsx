0b3aed1bbc4bb890be58af4020a283755d17ce5b
Zach Banducci <zbducci@gmail.com>
2019-04-27 11:10:47 -0500

    Restructured CurrentPlaying

--- src/pages/CurrentPlaying.jsx
+++ src/pages/CurrentPlaying.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { Subscribe } from 'unstated';
 
 import { bumpSong } from '../data/api';
-import { RoomContainer } from '../store/room';
 
 import BumpList from '../components/Songs/BumpList';
 import CurrentSong from '../components/Songs/CurrentSong';
 
-const CurrentPlaying = () => (
-  <Subscribe to={[RoomContainer]}>
-    {
-      room => (
-        <div style={{
-          width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
-        }}
-        >
-          <h2>Current Song</h2>
-          {
-            Object.keys(room.state.currentSong).length !== 0 ? (
-              <CurrentSong {...room.state.currentSong} />
-            ) : (
-              <h3>No Song</h3>
-            )
-          }
-          <h3>Current Queue</h3>
-          <BumpList
-            songs={room.state.queue}
-            onBump={song => bumpSong(room.state.name, 'Jim', song.id)}
-          />
-        </div>
-      )
-    }
-  </Subscribe>
-);
+const CurrentPlaying = (props) => {
+  const {
+    room,
+  } = props;
+
+  const handleBump = song => bumpSong(room.state.name, 'Jim', song.id);
+
+  return (
+    <div style={{
+      width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+    }}
+    >
+      <h2>Current Song</h2>
+      {
+        Object.keys(room.state.currentSong).length !== 0 ? (
+          <CurrentSong {...room.state.currentSong} />
+        ) : (
+          <h3>No Song</h3>
+        )
+      }
+      <h3>Current Queue</h3>
+      <BumpList
+        songs={room.state.queue}
+        onBump={handleBump}
+      />
+    </div>
+  );
+};
 
 export default CurrentPlaying;
