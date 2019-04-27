af1182bb6e04ad4251c7dc6e2559cb9d0043c2ef
Zach Banducci <zbducci@gmail.com>
2019-04-27 10:32:50 -0500

    Removed unnecc components

--- src/components/Column.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-export default Column;
