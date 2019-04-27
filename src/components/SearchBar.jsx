c90ac08be36d502981de85ba8c60ac03bbffc3f6
Zach Banducci <zbducci@gmail.com>
2019-04-27 10:54:00 -0500

    SearchBar cleanup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { IoIosSearch as SearchIcon } from 'react-icons/io';
 import styled from 'styled-components';
 
-import Input from './Input';
+const Input = styled.input`
+  height: 1.5em;
+  flex: 10;
+  padding-left: 20px;
+  margin: 0 0.5em;
+  font-size: 16px;
+  border: 1px solid #D6D6D6;
+  border-radius: 20px;
+  
+  :focus {
+    border: 1px solid #09A7FF;
+  }
+`;
 
 const Bar = styled.div`
   width: 100%;
@@ -21,12 +33,18 @@ const SearchBar = (props) => {
     onChange,
   } = props;
 
+  const handleInput = e => onChange(e.target.value);
+
   return (
     <Bar>
       <SearchIcon style={{ margin: '0 0 0 0.5em', flex: 1 }} />
-      <Input type="text" placeholder="Search..." onChange={onChange} />
+      <Input
+        type="text"
+        placeholder="Search..."
+        onChange={handleInput}
+      />
     </Bar>
   );
 };
 
-export default SearchBar;
+export default memo(SearchBar);
