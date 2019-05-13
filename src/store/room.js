b9493c568a8a827a202a7450622b7713ced2223a
Zach Banducci <zbducci@gmail.com>
2019-05-12 23:04:00 -0500

    unstated next added to room store

--- src/store/room.js
+++ src/store/room.js
@@ -1,82 +1,157 @@
-import { Container } from 'unstated'
+/* global EventSource localStorage */
+import { useEffect, useRef, useReducer } from 'react';
+import { createContainer } from 'unstated-next';
 
 const superBump = (queue) => {
-  queue.sort((a, b) => b.bumps - a.bumps) 
+  queue.sort((a, b) => b.bumps - a.bumps);
+};
+
+const initialState = {
+  queue: [],
+  currentSong: {},
+  members: [],
+};
+
+function init(state, store, alreadyBumped) {
+  const { current_song: currentSong, ...rest } = store;
+  const queue = rest.queue.map(song => ({
+    ...song,
+    alreadyBumped: alreadyBumped[song.id] || false,
+  }));
+
+  const song = typeof currentSong === 'string' ? {} : currentSong;
+  return {
+    ...state, ...rest, queue, currentSong: song,
+  };
 }
 
-const regularBump = (queue, index) => {
-  if(index !== 0)
-    [queue[index], queue[index - 1]] = [queue[index - 1], queue[index]]
+function setName(state, name) {
+  return { ...state, name };
 }
 
-export default class RoomContainer extends Container {
-  state = {
-		currentSong: {},
-		queue: [],
-		members: [],
-    name: ''
+function addSong(state, song) {
+  if (state.queue.length === 0 && !state.currentSong.uri) {
+    return { ...state, currentSong: song };
   }
+  return { ...state, queue: [...state.queue, song] };
+}
 
-  initRoom = store => {
-    const { current_song, ...rest } = store
-    const jsonMap = sessionStorage.getItem('alreadyBumped') || "{}"
-    const alreadyBumped = JSON.parse(jsonMap)
-    const queue = store.queue.map(song => ({
-      ...song, 
-      alreadyBumped: alreadyBumped[song.id] || false
-    }))
-
-    this.setState(prevState => ({...prevState, ...rest, queue, currentSong: current_song}))
+function addMember(state, member) {
+  if (!state.members.find(el => el === member)) {
+    return { ...state, members: [...state.members, member] };
   }
+  return state;
+}
 
-  setName = name => this.setState({name })
+function bumpSong(state, id) {
+  const { queue } = state;
+  const newQueue = queue.map((song) => {
+    if (song.id !== id) return song;
 
-  addToQueue = song => {
-    if(this.state.queue.length === 0 && !this.state.currentSong.hasOwnProperty('uri')) { 
-    } else {
-      this.setState(prevState => ({
-        queue: [...prevState.queue, song]
-      }))
-    }
-  }
+    return {
+      ...song,
+      bumps: song.bumps + 1,
+      alreadyBumped: true,
+    };
+  });
 
-  nextSong = () => {
-    if(this.state.queue.length > 0) {
-      const jsonMap = sessionStorage.getItem('alreadyBumped') || "{}"
-      const alreadyBumped = JSON.parse(jsonMap)
-      delete alreadyBumped[this.state.queue[0].id]
-      sessionStorage.setItem('alreadyBumped', JSON.stringify(alreadyBumped))
+  superBump(newQueue);
 
-      this.setState(prevState => ({
-        currentSong: prevState.queue[0], 
-        queue: prevState.queue.slice(1, prevState.queue.length)
-      }))
-    }
-  }
+  return { ...state, queue: newQueue };
+}
 
-  bumpSong = (id) => {
-    const index = this.state.queue.findIndex(song => song.id === id)
-    const queue = this.state.queue 
-    const jsonMap = sessionStorage.getItem('alreadyBumped') || "{}"
-    const alreadyBumped = JSON.parse(jsonMap)
-    
-    queue[index] = {
-      ...queue[index], 
-      bumps: queue[index].bumps + 1,
-      alreadyBumped: alreadyBumped[id] || false
-    }
+function nextSong(state, alreadyBumped) {
+  if (state.queue.length === 0) return state;
 
-    superBump(queue)
+  const { id } = state.queue[0];
+  const { [id]: omit, ...rest } = alreadyBumped;
+  localStorage.setItem('alreadyBumped', JSON.stringify(rest));
 
-    this.setState(prevState => ({
-      queue
-    }))
-  }
+  return {
+    ...state,
+    currentSong: state.queue[0],
+    queue: state.queue.slice(1, state.queue.length),
+  };
+}
 
-  addMember = member => {
-    if(!this.state.members.find(el => el === member))
-      this.setState(prevState => ({
-        members: [...prevState.members, member]
-      }))
+function reducer(state, action) {
+  switch (action.type) {
+    case 'init':
+      return init(state, action.payload, action.alreadyBumped);
+    case 'bump':
+      return bumpSong(state, action.payload);
+    case 'add':
+      return addSong(state, action.payload);
+    case 'join':
+      return addMember(state, action.payload);
+    case 'next':
+      return nextSong(state, action.alreadyBumped);
+    case 'name':
+      return setName(state, action.payload);
+    case 'reset':
+      localStorage.clear();
+      return initialState;
+    default:
+      return state;
   }
 }
+
+function useRoomContainer() {
+  const [room, dispatch] = useReducer(reducer, initialState);
+  const eventSource = useRef(null);
+
+  useEffect(() => {
+    async function getAlreadyBumped() {
+      const alreadyBumped = localStorage.getItem('alreadyBumped') || {};
+      return JSON.parse(alreadyBumped);
+    }
+
+    if (room.name) {
+      eventSource.current = new EventSource(`http://52.42.15.3:5000/stream?channel=${room.name}`);
+
+      eventSource.current.addEventListener(
+        'song',
+        ({ data }) => {
+          const { song } = JSON.parse(data);
+          dispatch({ type: 'add', payload: song });
+        },
+        false,
+      );
+
+      eventSource.current.addEventListener(
+        'join',
+        ({ data }) => {
+          const { user } = JSON.parse(data);
+          dispatch({ type: 'join', payload: user });
+        },
+        false,
+      );
+
+      eventSource.current.addEventListener(
+        'bump',
+        ({ data }) => {
+          dispatch({ type: 'bump', payload: data });
+        },
+        false,
+      );
+
+      eventSource.current.addEventListener(
+        'next',
+        () => {
+          const alreadyBumped = getAlreadyBumped();
+          dispatch({ type: 'next', alreadyBumped });
+        },
+        false,
+      );
+
+      return function unMount() {
+        eventSource.current.removeAllListeners();
+        eventSource.current.close();
+      };
+    }
+  }, [room.name, dispatch]);
+
+  return { room, dispatch };
+}
+
+export default createContainer(useRoomContainer);
