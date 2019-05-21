626409c8df183249c048adcf39693c4af936a643
Zach Banducci <zbducci@gmail.com>
2019-05-20 18:32:49 -0700

    Room Container in members page

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+
+import RoomContainer from '../store/room';
 import MemberList from '../components/MemberList';
 
-const Members = (props) => {
-  const {
-    room,
-  } = props;
-
+const Members = () => {
+  const { room } = RoomContainer.useContainer();
   return (
     <div>
       <h1>Members</h1>
-      <MemberList members={room.state.members} />
+      <MemberList members={room.members} />
     </div>
   );
 };
