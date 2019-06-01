44103bb4b124db2b4b08e51b929e1751181f1664
Zach Banducci <zbducci@gmail.com>
2019-06-01 16:52:10 +0100

    Image component that properly mounts and fades in

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { visible } from 'ansi-colors';
+import React, { memo, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const Background = styled.div`
+  background-color: #DBDAED;
+  width: ${props => props.dim}px;
+  height: ${props => props.dim}px;
+`;
+
+const Img = styled.img`
+  display: ${props => (props.loading ? 'none' : 'inline-block')};
+  width: ${props => props.dim}px;
+  height: ${props => props.dim}px;
+  animation: ${props => !props.loading && fadeIn};
+  animation-duration: 0.75s;
+  animation-timing-function: ease-in-out;
+`;
 
 const Image = (props) => {
-  const { 
+  const {
     src,
     alt,
-    fallback,
+    dim = 130,
     style,
   } = props;
 
   const [loading, setLoading] = useState(true);
-
-  const handleOnLoad = () => setLoading(false);
+  const handleLoad = () => setLoading(false);
   return (
-    <>
-      { loading && <img src={fallback} style={style}/> }
-      <img onLoad={handleOnLoad} src={src} style={{...style, display: `${ loading ? 'none' : 'block' }`}}/>
-    </>
-  )
-}
+    <Background dim={dim}>
+      <Img dim={dim} loading={loading} src={src} alt={alt} onLoad={handleLoad} style={style} />
+    </Background>
+  );
+};
 
-export default Image;
+export default memo(Image);
