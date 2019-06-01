75d0ccb8a666096b60f58299b48744064f49f0f9
Zach Banducci <zbducci@gmail.com>
2019-06-01 16:53:08 +0100

    Linted Member component

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -5,26 +5,26 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const Profile = styled.img`
   witdh: 200px;
   height: 200px;
   border-radius: 50%;
-`
+`;
 
 const Info = styled.div`
   margin: 1em;
-`
+`;
 
 const Name = styled.div`
   font-weight: bold;
   font-size: 1.2em;
-`
+`;
 
 const Username = styled.div`
   font-style: italic;
-`
+`;
 
 const Member = (props) => {
   const {
@@ -35,10 +35,10 @@ const Member = (props) => {
   } = props;
 
   const name = `${first} ${last}`;
-  
+
   return (
     <Container>
-      <Profile src={ image } alt={ name }/>
+      <Profile src={image} alt={name} />
       <Info>
         <Name>{ name }</Name>
         <Username>{ id }</Username>
