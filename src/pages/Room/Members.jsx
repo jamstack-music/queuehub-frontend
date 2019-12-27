27d4a706be12e25234839254ec7a49e0ebd33630
Zachary Banducci <zbducci@gmail.com>
2019-12-27 11:24:16 -0800

    Added basic room retrieval functionality in Room and Members

--- src/pages/Room/Members.jsx
+++ src/pages/Room/Members.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSelector } from 'AppState/react';
 
-const Members = () => (
-  <div>Members</div>
-);
+const Members = () => {
+  const members = useSelector((s) => s.members.all);
+
+  const memberList = useMemo(() => {
+    const memberItems = members.map((member) => (
+      <li key={member.id}>
+        <div>{member.name}</div>
+        <div>{member.username}</div>
+      </li>
+    ));
+
+    return <ul>{memberItems}</ul>;
+  }, [members]);
+
+  return (
+    <div>
+      <div>Members</div>
+      {memberList}
+    </div>
+  );
+};
 
 export default Members;
--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {
   Route,
   Redirect,
@@ -9,6 +9,7 @@ import {
 import { useDispatch } from 'AppState/react';
 import { RoomChannelProvider } from 'Components/RoomChannel';
 
+import useFetch from 'API/useFetch';
 import { retrieveRoom } from 'API/room';
 
 import Nav from 'Components/Nav';
@@ -22,26 +23,20 @@ import NotFound from '../NotFound';
 
 const Room = () => {
   const match = useRouteMatch('/room/:id');
+  const { fetch, isLoading, error } = useFetch();
   const dispatch = useDispatch();
-  const [isError, setisError] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    retrieveRoom(match.params.id)
-      .then((roomData) => {
-        dispatch({ type: 'initRoom', payload: roomData });
-        setLoading(false);
-      })
-      .catch((errorResp) => {
-        setisError(errorResp);
-      });
-  }, [dispatch, match.params.id]);
+    fetch(retrieveRoom, match.params.id).then((data) => {
+      if (data) dispatch({ type: 'initRoom', payload: data });
+    });
+  }, [dispatch, fetch, match.params.id]);
 
-  if (isError) {
+  if (error) {
     return <Redirect to="/" />;
   }
 
-  if (loading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <RoomChannelProvider id={match.params.id}>
